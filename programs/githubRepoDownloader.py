a2b2eb6d2a0cf145bf21ebd0a67917099100bb92
davidvelascogarcia <davelasc@ing.uc3m.es>
2021-02-07 16:19:29 +0100

    Transformation to class, halo, requirements.txt support added

--- programs/githubRepoDownloader.py
+++ programs/githubRepoDownloader.py
@@ -10,156 +10,185 @@
 import datetime
 from github import Github
 import os
+from halo import Halo
 import platform
-import sys
-import time
 
-print("")
-print("")
-print("**************************************************************************")
-print("**************************************************************************")
-print("                   Program: Github Repo Downloader                        ")
-print("                     Author: David Velasco Garcia                         ")
-print("                             @davidvelascogarcia                          ")
-print("**************************************************************************")
-print("**************************************************************************")
 
-print("")
-print("Starting system ...")
-print("")
+class GithubRepoDownloader:
 
-print("")
-print("Loading githubRepoDownloader engine ...")
-print("")
+    # Function: Constructor
+    def __init__(self):
 
-# Get system configuration
-print("")
-print("Detecting system and release version ...")
-print("")
-systemPlatform = platform.system()
-systemRelease = platform.release()
+        # Build Halo spinner
+        self.systemResponse = Halo(spinner='dots')
 
-print("")
-print("**************************************************************************")
-print("Configuration detected:")
-print("**************************************************************************")
-print("")
-print("Platform:")
-print(systemPlatform)
-print("Release:")
-print(systemRelease)
+    # Function: getSystemPlatform
+    def getSystemPlatform(self):
 
-# Get initial time
-initialTime = datetime.datetime.now()
+        # Get system configuration
+        print("\nDetecting system and release version ...\n")
+        systemPlatform = platform.system()
+        systemRelease = platform.release()
 
-print("")
-print("**************************************************************************")
-print("Enter username")
-print("**************************************************************************")
-print("")
-print("[INFO] Please, enter username:")
-print("")
+        print("**************************************************************************")
+        print("Configuration detected:")
+        print("**************************************************************************")
+        print("\nPlatform:")
+        print(systemPlatform)
+        print("Release:")
+        print(systemRelease)
 
-# Get username
-githubUsername = input("Username: ")
+        return systemPlatform, systemRelease
 
-print("")
-print("**************************************************************************")
-print("Searching Repositories")
-print("**************************************************************************")
-print("")
-print("[INFO] Searching " + str(githubUsername) + " repositories ...")
-print("")
+    # Function: getUser
+    def getUser(self):
 
-# Create github client and get username repos
-try:
-    # Build githubClient
-    githubClient = Github()
+        print("**************************************************************************")
+        print("Enter username")
+        print("**************************************************************************")
 
-    # Get github user profile
-    githubUser = githubClient.get_user(str(githubUsername))
+        systemResponseMessage = "\n[INFO] Please, enter username:\n"
+        self.systemResponse.text_color = "blue"
+        self.systemResponse.info(systemResponseMessage)
 
-    # Get githubUser repositorieslist
-    githubUserRepos = githubUser.get_repos()
+        # Get username
+        githubUsername = input()
 
-except:
-    print("")
-    print("[ERROR] Error getting github user repositories.")
-    print("")
+        return githubUsername
 
-    # Move to root path
-try:
-    os.chdir("./../../")
+    # Function: repoDownloader
+    def repoDownloader(self, githubUsername):
 
-except:
-    print("")
-    print("[ERROR] Error moving to root path.")
-    print("")
+        # Get initial time
+        initialTime = datetime.datetime.now()
 
-# Variable to count repo downloaded
-repoDownloadedCount = 1
+        print("\n**************************************************************************")
+        print("Searching Repositories")
+        print("**************************************************************************")
 
-# Download all user repositories
-for repositorie in githubUserRepos:
+        systemResponseMessage = "\n[INFO] Searching " + str(githubUsername) + " repositories ...\n"
+        self.systemResponse.text_color = "yellow"
+        self.systemResponse.warn(systemResponseMessage)
 
-    print("")
-    print("[INFO] Repositorie founded: " + str(repositorie.name))
-    print("")
+        # Create github client and get username repos
+        try:
+            # Build githubClient
+            githubClient = Github()
 
-    print("")
+            # Get github user profile
+            githubUser = githubClient.get_user(str(githubUsername))
+
+            # Get githubUser repositories list
+            githubUserRepos = githubUser.get_repos()
+
+        except:
+
+            systemResponseMessage = "\n[ERROR] Error getting github user repositories.\n"
+            self.systemResponse.text_color = "red"
+            self.systemResponse.fail(systemResponseMessage)
+
+        # Move to root path
+        try:
+            os.chdir("./../../")
+
+        except:
+
+            systemResponseMessage = "\n[ERROR] Error moving to root path.\n"
+            self.systemResponse.text_color = "red"
+            self.systemResponse.fail(systemResponseMessage)
+
+        # Variable to count repo downloaded
+        repoDownloadedCount = 1
+
+        # Download all user repositories
+        for repo in githubUserRepos:
+
+            systemResponseMessage = "\n[INFO] Repo founded: " + str(repo.name) + "\n"
+            self.systemResponse.text_color = "yellow"
+            self.systemResponse.warn(systemResponseMessage)
+
+            # Execute repo clone
+            try:
+                print("\n**************************************************************************")
+                print("Downloading repo " + str(repo.name))
+                print("**************************************************************************\n")
+
+                systemResponseMessage = "\n[INFO] Downloading repo " + str(repo.name) + ", " + str(repoDownloadedCount) + "/" + str(githubUserRepos.totalCount) + " ...\n"
+                self.systemResponse.text_color = "blue"
+                self.systemResponse.info(systemResponseMessage)
+
+                # Prepare github repo path and clone command
+                githubRepoPath = "https://github.com/" + str(githubUsername) + "/" + str(repo.name)
+                cloneCommand = "git clone " + str(githubRepoPath)
+
+                os.system(str(cloneCommand))
+
+                systemResponseMessage = "\n[INFO] " + str(repo.name) + " repo cloned correctly.\n"
+                self.systemResponse.text_color = "green"
+                self.systemResponse.succeed(systemResponseMessage)
+
+            except:
+                os.system(str(cloneCommand))
+
+                systemResponseMessage = "\n[ERROR] Error cloning " + str(repo.name) + " repo.\n"
+                self.systemResponse.text_color = "red"
+                self.systemResponse.fail(systemResponseMessage)
+
+            # Count repoDownloadedCount + 1
+            repoDownloadedCount = repoDownloadedCount + 1
+
+        print("\n**************************************************************************")
+        print(str(githubUsername) + " Repositories Cloned")
+        print("**************************************************************************")
+
+        systemResponseMessage = "\n[INFO] " + str(githubUsername) + " repositories cloned correctly.\n"
+        self.systemResponse.text_color = "green"
+        self.systemResponse.succeed(systemResponseMessage)
+
+        # Get end time
+        endTime = datetime.datetime.now()
+
+        # Get elapsedTime
+        elapsedTime = endTime - initialTime
+
+        systemResponseMessage = "\n[INFO] Elapsed time: " + str(elapsedTime)
+        self.systemResponse.text_color = "blue"
+        self.systemResponse.info(systemResponseMessage)
+
+
+# Function: main
+def main():
+
     print("**************************************************************************")
-    print("Downloading repositorie " + str(repositorie.name))
     print("**************************************************************************")
-    print("")
-    print("[INFO] Downloading repositorie " + str(repositorie.name) + ", " + str(repoDownloadedCount) + "/" + str(githubUserRepos.totalCount) + " ...")
-    print("")
+    print("                   Program: Github Repo Downloader                        ")
+    print("                     Author: David Velasco Garcia                         ")
+    print("                             @davidvelascogarcia                          ")
+    print("**************************************************************************")
+    print("**************************************************************************")
 
-    # Prepare github repositorie path
-    githubRepoPath = "https://github.com/" + str(githubUsername) + "/" + str(repositorie.name)
+    print("\nLoading Github Repo Downloader engine ...\n")
 
-    # Prepare cloneCommand
-    cloneCommand = "git clone " + str(githubRepoPath)
+    # Build githubRepoDownloader object
+    githubRepoDownloader = GithubRepoDownloader()
 
-    # Execute repositorie clone
-    try:
-        os.system(str(cloneCommand))
+    # Get system platform
+    systemPlatform, systemRelease = githubRepoDownloader.getSystemPlatform()
 
-        print("")
-        print("[INFO] " + str(repositorie.name) + " repositorie cloned correctly.")
-        print("")
+    # Set Github username
+    username = githubRepoDownloader.getUser()
 
-    except:
-        print("")
-        print("[ERROR] Error cloning " + str(repositorie.name) + " repositorie.")
-        print("")
+    # Get Github repos
+    githubRepoDownloader.repoDownloader(username)
 
-    # Count repoDownloadedCount + 1
-    repoDownloadedCount = repoDownloadedCount + 1
+    print("\n**************************************************************************")
+    print("Program finished:")
+    print("**************************************************************************")
+    print("\ngithubRepoDownloader program finished correctly.\n")
 
-print("")
-print("**************************************************************************")
-print(str(githubUsername) + " Repositories Cloned")
-print("**************************************************************************")
-print("")
-print("[INFO] " + str(githubUsername) + " repositories cloned correctly.")
-print("")
+    userExit = input()
 
-# Get end time
-endTime = datetime.datetime.now()
+if __name__ == "__main__":
 
-# Get elapsedTime
-elapsedTime = endTime - initialTime
-
-print("")
-print("[INFO] Elapsed time: " + str(elapsedTime))
-print("")
-
-print("")
-print("")
-print("**************************************************************************")
-print("Program finished")
-print("**************************************************************************")
-print("")
-print("githubRepoDownloader program finished correctly. ")
-print("")
-endProgram = input()
+    # Call main function
+    main()
